1c7655ec01c0690c0b708716bbef994dc8533d79
Anshul <anbirla@ucsd.edu>
2022-01-03 23:19:43 -0800

    changed structure added linting

---
+++ scripts/migrate.ts
@@ -0,0 +1,23 @@
+// make sure to run this when you are in the lap_student_tracker directory in your command line
+
+const { Pool } = require("pg");
+const { migrate } = require("postgres-migrations");
+
+const client = new Pool({
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: "postgres",
+  password: process.env.DB_PASS,
+  port: 5432,
+});
+
+const runMigration = async () => {
+  try {
+    await migrate({ client }, "./migrations");
+    console.log("Migration Ran Succesfully");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+runMigration().then(() => process.exit());
